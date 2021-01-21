d4036039d9eeb641dff444e2ed81047dc3c12a26
mpmcauliffe <mcauliffe99@gmail.com>
2021-01-20 18:29:03 -0700

    create auth routes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express           = require('express')
 const mongoose          = require('mongoose')
 const dotenv            = require('dotenv')
 const morgan            = require('morgan')
-const exphbs            = require('express-handlebars')
+// const exphbs            = require('express-handlebars')
 const passport          = require('passport')
 const session           = require('express-session')
 const MongoStore        = require('connect-mongo')(session)
@@ -29,16 +29,16 @@ app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 /* 🍔 helper */
-const { formatDate, truncate, stripTags, editIcon, select, } = require('./helpers/hbs')
-// handlebars
-app.engine(
-    '.hbs', 
-    exphbs({ 
-        helpers: { formatDate, stripTags, truncate, editIcon, select, }, 
-        defaultLayout: 'main', 
-        extname: '.hbs' 
-    }))
-app.set('view engine', '.hbs')
+// const { formatDate, truncate, stripTags, editIcon, select, } = require('./helpers/hbs')
+// // handlebars
+// app.engine(
+//     '.hbs', 
+//     exphbs({ 
+//         helpers: { formatDate, stripTags, truncate, editIcon, select, }, 
+//         defaultLayout: 'main', 
+//         extname: '.hbs' 
+//     }))
+// app.set('view engine', '.hbs')
 
 // session
 app.use(session({
