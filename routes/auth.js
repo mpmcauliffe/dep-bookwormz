6e831206becc4d154481c2aef71270cb789745d0
mpmcauliffe <mcauliffe99@gmail.com>
2021-02-11 05:08:47 -0700

    attempt - 5

--- routes/auth.js
+++ routes/auth.js
@@ -54,10 +54,10 @@ router.get('/google/callback',
     passport.authenticate('google', { failureRedirect: '/' }),
     
     (req, res) => {
-        process.env.NODE_ENV === 'production'
-            ? res.redirect('https://bookwormz-api.herokuapp.com/userauth')
-            : res.redirect('http://localhost:3000/userauth')
-        // res.redirect('https://bookwormz-api.herokuapp.com/userauth')
+        // process.env.NODE_ENV === 'production'
+        //     ? res.redirect('https://bookwormz-api.herokuapp.com/userauth')
+        //     : res.redirect('http://localhost:3000/userauth')
+        res.redirect('https://bookwormz-api.herokuapp.com/userauth')
         // res.redirect('http://localhost:3000') 
         // res.redirect('https://agitated-shannon-3f318f.netlify.app/') 
     }
