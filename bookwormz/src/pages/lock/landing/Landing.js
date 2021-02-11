604a94cd81b36c8216b0e4d55a28a1744fed1e26
mpmcauliffe <mcauliffe99@gmail.com>
2021-02-11 03:08:39 -0700

    attempt 3 - updated Landing page on client ... still attempting redirect to .../auth/google & .../userauth

--- bookwormz/src/pages/lock/landing/Landing.js
+++ bookwormz/src/pages/lock/landing/Landing.js
@@ -26,7 +26,9 @@ const Landing_proto = ({ isAuthenticated, error, history }) => {
             </section>
             
             <section className='container__links'>
-                <a href='http://localhost:5000/auth/google' 
+                <a 
+                    // href='http://localhost:5000/auth/google' 
+                    href='/auth/google' 
                     className='link__btn link__btn-google'>
                     <i className='fab fa-google left fa-1x'>
                         </i>&nbsp;&nbsp;&nbsp; Log in with Google
