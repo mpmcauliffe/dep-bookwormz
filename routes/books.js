65a05f3a4fa55cd8279cb259df4ecdda3e22b0ab
mpmcauliffe <mcauliffe99@gmail.com>
2021-02-17 02:53:14 -0700

    attempt 4 - no verification :(

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,7 @@ const API_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 const router = express.Router()
 
 //verification,
-router.get('/booksearch/:urlSearchString', verification, (req, res) => {
+router.get('/booksearch/:urlSearchString', (req, res) => {
     console.log(req.params)
     const { urlSearchString } = req.params
     const searchString = urlSearchString.replace(/_/g, " ")
