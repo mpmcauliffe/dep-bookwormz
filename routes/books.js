348d8e9e663d86104f4f8a3fa22d2b13dbc36c05
mpmcauliffe <mcauliffe99@gmail.com>
2021-02-17 00:40:37 -0700

    attempt 2 - without verification on booksearch rte

--- routes/books.js
+++ routes/books.js
@@ -6,20 +6,13 @@ const verification = require('../middleware/verification')
 const API_URL = 'https://www.googleapis.com/books/v1/volumes?q='
 const router = express.Router()
 
-
-router.get('/booksearch/:urlSearchString', verification, (req, res) => {
-    //console.log(req.headers['x-auth-token'])
-    //const userId = getUserId
+//verification,
+router.get('/booksearch/:urlSearchString', (req, res) => {
     console.log(req.params)
     const { urlSearchString } = req.params
     const searchString = urlSearchString.replace(/_/g, " ")
 
-    //res.send({ "bears": "cute" })
-
     needle.get(`${API_URL}${searchString}`, (err, response) => {
-        //console.log(err)
-        //console.log(response)
-        //console.log(response.body)
         res.json(response.body)
     })
 })
