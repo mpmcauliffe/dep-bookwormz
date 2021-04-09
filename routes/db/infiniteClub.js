87d764b1e7aef3ac37a83564c26cc66c5c5e1685
mpmcauliffe <mcauliffe99@gmail.com>
2021-04-08 23:32:00 -0600

    create dummie data for clubs

---
+++ routes/db/infiniteClub.js
@@ -0,0 +1,219 @@
+module.exports = [
+    {
+        "_id": {
+          "$oid": "604efae6e2ad40f8bd455626"
+        },
+        "books": [
+          ""
+        ],
+        "email": "bullwinkle@rocky.com",
+        "userId": "",
+        "displayName": "Bullwinkle",
+        "firstName": "Bullwinkle",
+        "lastName": "Moose",
+        "image": "./img/mock/bullwinkle.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-15T00:16:58.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "604efd98e2ad40f8bd455627"
+        },
+        "books": [
+          ""
+        ],
+        "email": "kaneda@akira.com",
+        "userId": "",
+        "displayName": "Kaneda",
+        "firstName": "Kaneda",
+        "lastName": "Shotaro",
+        "image": "./img/mock/kaneda.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-15T00:16:58.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605025785e6e8834ebceb5f0"
+        },
+        "books": [
+          ""
+        ],
+        "email": "izzy@bear.com",
+        "userId": "",
+        "displayName": "Izzy",
+        "firstName": "Izzy",
+        "lastName": "Bear",
+        "image": "./img/mock/izzy.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605031675e6e8834ebceb5f2"
+        },
+        "books": [
+          ""
+        ],
+        "email": "amanda@bear.com",
+        "userId": "",
+        "displayName": "Amanda the Panda",
+        "firstName": "Amanda",
+        "lastName": "The Panda",
+        "image": "./img/mock/panda.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605039365e6e8834ebceb5f7"
+        },
+        "books": [
+          ""
+        ],
+        "email": "sefinaz@bear.com",
+        "userId": "",
+        "displayName": "Sefinaz",
+        "firstName": "Sefinaz",
+        "lastName": "Bear",
+        "image": "./img/mock/sun.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605039f35e6e8834ebceb5f9"
+        },
+        "books": [
+          ""
+        ],
+        "email": "paddy@bear.com",
+        "userId": "",
+        "displayName": "Paddy",
+        "firstName": "Paddy",
+        "lastName": "Bear",
+        "image": "./img/mock/andean.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "60507ca6d68a71b43d056c7a"
+        },
+        "books": [
+          ""
+        ],
+        "email": "et@alien.com",
+        "userId": "",
+        "displayName": "E.T.",
+        "firstName": "Extra",
+        "lastName": "Terrestrial",
+        "image": "./img/mock/et.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "60507d1cd68a71b43d056c7c"
+        },
+        "books": [
+          ""
+        ],
+        "email": "lange@econ.com",
+        "userId": "",
+        "displayName": "Oskar Lange",
+        "firstName": "Oskar",
+        "lastName": "Lange",
+        "image": "./img/mock/lange.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "60507d64d68a71b43d056c7d"
+        },
+        "books": [
+          ""
+        ],
+        "email": "hayek@alien.com",
+        "userId": "",
+        "displayName": "Ludwig von Hayek",
+        "firstName": "Ludwig",
+        "lastName": "Hayek",
+        "image": "./img/mock/hayek.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "6050808ad68a71b43d056c7e"
+        },
+        "books": [
+          ""
+        ],
+        "email": "einstien@phys.com",
+        "userId": "",
+        "displayName": "Einstein",
+        "firstName": "Albert",
+        "lastName": "Einstein",
+        "image": "./img/mock/einstein.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605080d7d68a71b43d056c7f"
+        },
+        "books": [
+          ""
+        ],
+        "email": "feynman@phys.com",
+        "userId": "",
+        "displayName": "Feynman",
+        "firstName": "Richard",
+        "lastName": "Feynman",
+        "image": "./img/mock/feynman.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "60508109d68a71b43d056c80"
+        },
+        "books": [
+          ""
+        ],
+        "email": "bohr@phys.com",
+        "userId": "",
+        "displayName": "Bohr",
+        "firstName": "Neils",
+        "lastName": "Bohr",
+        "image": "./img/mock/bohr.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      }
+]
