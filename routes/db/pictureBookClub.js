257693557330714d7e2dea9adc6e3bf0870faf46
mpmcauliffe <mcauliffe99@gmail.com>
2021-04-08 23:41:58 -0600

    update dummie data

---
+++ routes/db/pictureBookClub.js
@@ -0,0 +1,183 @@
+module.exports = [
+    {
+        "_id": {
+          "$oid": "604edd64e2ad40f8bd455624"
+        },
+        "books": [
+          ""
+        ],
+        "email": "jimbo@bear.com",
+        "userId": "",
+        "displayName": "Jimbo",
+        "firstName": "Jimbo",
+        "lastName": "Bear",
+        "image": "./img/mock/jimbo.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T10:11:12.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "604efae6e2ad40f8bd455626"
+        },
+        "books": [
+          ""
+        ],
+        "email": "bullwinkle@rocky.com",
+        "userId": "",
+        "displayName": "Bullwinkle",
+        "firstName": "Bullwinkle",
+        "lastName": "Moose",
+        "image": "./img/mock/bullwinkle.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-15T00:16:58.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "604efd98e2ad40f8bd455627"
+        },
+        "books": [
+          ""
+        ],
+        "email": "kaneda@akira.com",
+        "userId": "",
+        "displayName": "Kaneda",
+        "firstName": "Kaneda",
+        "lastName": "Shotaro",
+        "image": "./img/mock/kaneda.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-15T00:16:58.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605038545e6e8834ebceb5f3"
+        },
+        "books": [
+          ""
+        ],
+        "email": "jack@sparrow.com",
+        "userId": "",
+        "displayName": "Jack",
+        "firstName": "Jack",
+        "lastName": "Sparrow",
+        "image": "./img/mock/jacksparrow.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605038805e6e8834ebceb5f4"
+        },
+        "books": [
+          ""
+        ],
+        "email": "sepheroth@bigsword.com",
+        "userId": "",
+        "displayName": "Sepheroth",
+        "firstName": "Sepheroth",
+        "lastName": "",
+        "image": "./img/mock/sepheroth.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605038b55e6e8834ebceb5f5"
+        },
+        "books": [
+          ""
+        ],
+        "email": "selim@bear.com",
+        "userId": "",
+        "displayName": "Selim",
+        "firstName": "Selim",
+        "lastName": "Bear",
+        "image": "./img/mock/sloth.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605039365e6e8834ebceb5f7"
+        },
+        "books": [
+          ""
+        ],
+        "email": "sefinaz@bear.com",
+        "userId": "",
+        "displayName": "Sefinaz",
+        "firstName": "Sefinaz",
+        "lastName": "Bear",
+        "image": "./img/mock/sun.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605039765e6e8834ebceb5f8"
+        },
+        "books": [
+          ""
+        ],
+        "email": "kermit@bear.com",
+        "userId": "",
+        "displayName": "Kermit the Kermode",
+        "firstName": "Kermit",
+        "lastName": "Kermode",
+        "image": "./img/mock/kermode.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605039f35e6e8834ebceb5f9"
+        },
+        "books": [
+          ""
+        ],
+        "email": "paddy@bear.com",
+        "userId": "",
+        "displayName": "Paddy",
+        "firstName": "Paddy",
+        "lastName": "Bear",
+        "image": "./img/mock/andean.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },{
+        "_id": {
+          "$oid": "605054e9d68a71b43d056c77"
+        },
+        "books": [
+          ""
+        ],
+        "email": "semiramis@bear.com",
+        "userId": "",
+        "displayName": "Semiriamis",
+        "firstName": "Semiramis",
+        "lastName": "Bear",
+        "image": "./img/mock/polar.png",
+        "secondaryDisplayName": "",
+        "secondaryImage": "",
+        "createdAt": {
+          "$date": "2021-03-14T09:49:29.869Z"
+        }
+      },
+]
