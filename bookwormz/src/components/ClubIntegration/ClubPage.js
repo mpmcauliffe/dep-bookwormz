b1f92233e321f7e7bd2c846a75368e6ff3918481
mpmcauliffe <mcauliffe99@gmail.com>
2021-04-12 23:55:28 -0600

    update

--- bookwormz/src/components/ClubIntegration/ClubPage.js
+++ bookwormz/src/components/ClubIntegration/ClubPage.js
@@ -65,7 +65,13 @@ export const Club_proto = ({ getClub, currentClub, }) => {
                                         ? `${member.profile}` 
                                         : require(`./img/mock/${member.profile}.png`).default}
                                     alt='CLUB_MEMBER'/>
-                                    <p>{member.name}</p>
+                                    {member.chiefAdmin 
+                                        ? <p><i 
+                                            className='fas fa-user-shield'
+                                            style={{ color: '#c7c5c7', }} 
+                                        />&nbsp;&nbsp;{member.name}</p>
+                                        : <p>{member.name}</p>
+                                    }
                             </div>
                             
                         ))}
