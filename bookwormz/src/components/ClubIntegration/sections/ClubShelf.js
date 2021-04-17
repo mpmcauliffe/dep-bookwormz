5a3c11654fb8f54380ed52fbdb4af4c094a75fe5
mpmcauliffe <mcauliffe99@gmail.com>
2021-04-16 22:49:48 -0600

    update native club components

--- bookwormz/src/components/ClubIntegration/sections/ClubShelf.js
+++ bookwormz/src/components/ClubIntegration/sections/ClubShelf.js
@@ -1,35 +1,17 @@
-import React, { useState, useEffect, } from 'react'
-import { connect } from 'react-redux'
+import React from 'react'
 import PropTypes from 'prop-types'
 import { ClubBookShelf, } from '../../bookShelf/Books.comp'
-import { getClubBookShelf } from '../../../redux/actions/clubActions'
 
 
-export const ClubShelf_proto = ({ clubId, getClubBookShelf, clubBooks, }) => {
-
-    useEffect(() => {
-        // if (!clubBooks) { getClubBookShelf(clubId) }
-    })
-
-    return (
-        <div>
-            <ClubBookShelf>
-            
-            </ClubBookShelf>
-        </div>
+export const ClubShelf = ({ book, }) => (
+    <div>
+        <ClubBookShelf>
         
-    )
+        </ClubBookShelf>
+    </div>
+    
+)
+
+ClubShelf.propTypes = {
+    book: PropTypes.object.isRequired,
 }
-
-ClubShelf_proto.propTypes = {
-    getClubBookShelf: PropTypes.func.isRequired,
-    clubBooks: PropTypes.array,
-}
-
-const mapStateToProps = state => ({
-    getClubBookShelf: state.clubs.getClubBookShelf,
-    clubBooks: state.clubs.clubBooks,
-})
-
-const ClubShelf = connect(mapStateToProps, { getClubBookShelf, })(ClubShelf_proto)
-export { ClubShelf }
