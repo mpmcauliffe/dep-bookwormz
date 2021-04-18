c0e50e0bc247a5dba42817f9b4f984da9414b2c3
mpmcauliffe <mcauliffe99@gmail.com>
2021-04-18 04:29:07 -0600

    add club dock to dashboard

--- bookwormz/src/pages/Dashboard.js
+++ bookwormz/src/pages/Dashboard.js
@@ -5,15 +5,16 @@ import { motion, } from 'framer-motion'
 import { getUserInfo } from '../redux/actions/accountActions'
 import { getBooks, } from '../redux/actions/bookActions'
 import { pageTransition, pageVariants, } from './zAnimation'
-import { MyBookShelf, HeaderSection, MainContent, } from '../components'
+import { Buffer, ClubDock, MyBookShelf, HeaderSection, MainContent, StandarGrid } from '../components'
 
 
-export const Dashboard_proto = ({ getUserInfo, getBooks, displayName, }) => {
+export const Dashboard_proto = ({ getUserInfo, getBooks, displayName, myClubs, }) => {
 
     useEffect(() => { 
         getUserInfo()
         getBooks() 
-    })
+    }, [ ])
+    
 
     return (
         <motion.div
@@ -30,7 +31,24 @@ export const Dashboard_proto = ({ getUserInfo, getBooks, displayName, }) => {
                 </HeaderSection>
                 {/* <div style={{ height: '60rem', width: '100%', backgroundColor: '#dfdf22' }} /> */}
                 <h3 className='subName'>Welcome {displayName}</h3>
-                <h3>My Books</h3>
+
+                <h3>Your Clubs</h3>
+                <Buffer thickness={9} />
+                {myClubs.length < 1
+                    ?  <p>There aren't any book clubs at this time.</p>
+                    : (
+                        <StandarGrid id='book-cover-select'>
+                            {myClubs.map((club, i) => (
+                                <ClubDock
+                                    index={i}
+                                    club={club}
+                                    key={club._id} />
+                            ))}
+                        </StandarGrid>
+                )}
+                
+                <Buffer thickness={13} />
+                <h3>Your Books</h3>
                 <MyBookShelf />
             </MainContent>
             
@@ -44,10 +62,12 @@ Dashboard_proto.propTypes = {
     getUserInfo: PropTypes.func.isRequired,
     getBooks: PropTypes.func.isRequired,
     displayName: PropTypes.string.isRequired,
+    myClubs: PropTypes.array.isRequired,
 }
 
 const mapStateToProps = state => ({
     displayName: state.account.displayName,
+    myClubs: state.books.myClubs,
 })
 
 const Dashboard = connect(mapStateToProps, { getUserInfo, getBooks, })(Dashboard_proto)
