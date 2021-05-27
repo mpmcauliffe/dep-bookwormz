e0050c19beb3baaa631c93a4452b22d9cb8c0173
mpmcauliffe <mcauliffe99@gmail.com>
2021-05-26 20:59:00 -0600

    update comments data

---
+++ routes/db/commentFill/horrorClub.js
@@ -0,0 +1,25 @@
+module.exports = [
+    {
+        // _id: 'QIWOejnaknafu377',
+        memberId: '6071290a0a23f585405d87e4',
+        name: 'Melkor',
+        profile: 'melkor',
+        subject: 'Welcome',
+        content: 'After many millennia, I have escaped the void and I am back in Middle Earth to rule over this book club.',
+        createdOn: '15 MAY 2021',
+        color: '#fff7f7',
+        border: '#ddd7d7',
+    }, {
+        // _id: 'QnaOnafeuj7kI3W7',
+        memberId: '6071290a0a23f585405d87e6',
+        name: 'Jack',
+        profile: 'jack',
+        subject: 'Welcome',
+        content: 'Did you finish the rum?',
+        replyTo: 'uWa3If7anOnj7Qke',
+        replyToOrigin: 'uWa3If7anOnj7Qke',
+        createdOn: '15 MAY 2021',
+        color: '#fff7f7',
+        border: '#ddd7d7',
+    }, 
+]
