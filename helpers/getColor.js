07994f4ef92868fd27897f321d1f0c07c03ee24f
mpmcauliffe <mcauliffe99@gmail.com>
2021-06-07 23:42:30 -0600

    color generator

---
+++ helpers/getColor.js
@@ -0,0 +1,14 @@
+const standardColors = Object.freeze({
+    RED:   Symbol('fff7f7'),
+    GREEN: Symbol('f7fff7'),
+    BLUE:  Symbol('f7f7ff'),
+})
+const grayColors = Object.freeze({
+    RED:   Symbol('ddd7d7'),
+    GREEN: Symbol('d7ddd7'),
+    BLUE:  Symbol('d7d7dd'),
+})
+
+module.exports = (modifyingObject, shadeModifier) => {
+
+}
