b48bc87c78aebc1f2c306681974adfba0538c167
mpmcauliffe <mcauliffe99@gmail.com>
2022-10-15 21:04:11 -0400

    FONTEND: remove cookie path from logout action

--- bookwormz/src/redux/actions/authActions.js
+++ bookwormz/src/redux/actions/authActions.js
@@ -89,9 +89,9 @@ export const logout = history => async dispatch => {
         dispatch({ type: LOGOUT })
         // const name = 'connect.sid'
         // document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;'
-        const res = await axios.get('/auth/logout')
+        // const res = await axios.get('/auth/logout')
 
-        console.log(res)
+        // console.log(res)
                 
     } catch (e) {
         history.push('/dashboard')
